c0b26abf83fc8a0e8c54914ada99825956008f75
phuc <phuc@MacBook-Air-cua-phuc.local>
2025-07-03 20:55:26 +0900

    Add basic React UI for Voting DApp

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Voting DApp</h1>
+      <p>Connect your wallet to vote.</p>
+    </div>
+  );
+}
+
+export default App;
